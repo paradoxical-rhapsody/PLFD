bf068c8841f76dec24851cf7d0b252e7f031aeb1
zengc <xzcdyx@outlook.com>
2021-08-07 22:51:19 +0800

    update `plfd.R` and `predict-plfd.R`

--- R/predict-plfd.R
+++ R/predict-plfd.R
@@ -2,27 +2,37 @@
 #' 
 #' @param object `plfd` object.
 #' @param x The samples to be predicted.
+#' @param y Vector. Labels of x (optional) with value `1` or `2`.
 #' @param ... Ignored currently.
 #' 
-#' @return Matrix with two columns, including the scores and predicted groups.
+#' @return `list(W, y.hat, mcr)`, wherein `W` refers to the discriminant 
+#' scores, `y.hat` refers to the predicted labels and `mcr` is the misclassification
+#' rate when `y` is available.
 #' @export 
-predict.plfd <- function(object, x, ...) {
-    stopifnot(object$rDim == NROW(x))
-    stopifnot(object$cDim == NCOL(x))
+predict.plfd <- function(object, x, y, ...) {
+    stopifnot( object$rDim == NROW(x) )
+    stopifnot( object$cDim == NCOL(x) )
+
     if (is.matrix(x)) dim(x) <- c(NROW(x), NCOL(x), 1)
     n <- dim(x)[3]
   
     W <- rep(0.0, n)
-    for (iB in 1:length(object$paras)) {
+    for (iB in seq(object$paras)) {
       rIdx <- object$paras[[iB]]$rIdx
       cIdx <- object$paras[[iB]]$cIdx
       M <- object$paras[[iB]]$M
       B <- object$paras[[iB]]$B
       W <- W + apply(x[rIdx, cIdx, , drop=FALSE], 3, function(.) sum((.-M) * B))
     }
-  
-    y <- ifelse(W>0, 1, 2)
-    result <- matrix(c(W, y), n, 2)
-    colnames(result) <- c('score', 'group')
+
+    y.hat=ifelse(W>0, 1, 2)
+    result <- list(score=W, y.hat=y.hat)
+
+    if (!is.missing(y)) {
+      stopifnot( n == length(y) ) 
+      stopifnot( all(unique(y) %in%  1:2) )
+      result["mcr"] = sum(y.hat != y) / n
+    }
+
     return(result)
 }

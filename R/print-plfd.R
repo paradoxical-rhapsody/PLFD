3dbd80643f0155bf34dc999d7b17e50b0d67f63a
zengc <xzcdyx@outlook.com>
2021-08-12 08:56:18 +0800

    update documentation of `print.plfd.R`

--- R/print-plfd.R
+++ R/print-plfd.R
@@ -2,10 +2,9 @@
 #' 
 #' @param x `plfd` object.
 #' @param ... Ignored currently.
-#' 
-#' @export
+#' @noRd 
 print.plfd <- function (x, ...) {
     cat(sprintf('Dimension of Matrix-vairate: %d x %d.\n', x$rDim, x$cDim))
     cat(sprintf('Training data: n1=%d, n2=%d.\n', x$n1, x$n2))
-    cat(sprintf('Number of feature block(s): %d.\n\n', length(x$paras)))
+    cat(sprintf('Number of feature block(s): %d.\n', length(x$paras)))
 }

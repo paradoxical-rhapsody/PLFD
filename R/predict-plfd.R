ce03b868bdbf331a3fe08a20de86e374bf38097a
zengc <xzcdyx@outlook.com>
2021-08-12 09:00:11 +0800

    update documentation of `predict-plfd.R`

--- R/predict-plfd.R
+++ R/predict-plfd.R
@@ -1,13 +1,14 @@
 #' @title Predict Method for `plfd`
 #' 
 #' @param object `plfd` object.
-#' @param x The samples to be predicted.
-#' @param y Vector (optional). Labels of x with value `1` or `2`.
+#' @param x Array, matrix-variate data to be predicted.
+#' @param y Vector (optional), Labels of `x` with value `1` or `2`.
 #' @param ... Ignored currently.
 #' 
-#' @return `list(W, y.hat, mcr)`, wherein `W` refers to the discriminant 
-#' scores, `y.hat` refers to the predicted labels and `mcr` is the misclassification
-#' rate when `y` is available.
+#' @return `list(W, y.hat, mcr)`, \itemize{
+#'  \item `W`: discriminant scores;
+#'  \item `y.hat`: predicted labels;
+#'  \item `mcr`: misclassification rate if parameter `y` is available.
 #' @export 
 predict.plfd <- function(object, x, y, ...) {
     stopifnot( object$rDim == NROW(x) )

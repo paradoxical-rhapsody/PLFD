04766aae831331c489cc5fe4dbedc64a0b656ced
zengchao@Dell-tower <xzcdyx@outlook.com>
2023-01-04 19:49:31 +0800

    update `print-plfd.R`

--- R/print-plfd.R
+++ R/print-plfd.R
@@ -4,7 +4,7 @@
 #' @param ... Ignored currently.
 #' @export
 print.plfd <- function (x, ...) {
-    cat(sprintf('Dimension of Matrix-vairate: %d x %d.\n', x$rDim, x$cDim))
-    cat(sprintf('Training data: n1=%d, n2=%d.\n', x$n1, x$n2))
-    cat(sprintf('Number of feature block(s): %d.\n', length(x$paras)))
+    message(sprintf('Dimension of Matrix-variate: %d x %d.', x$rDim, x$cDim))
+    message(sprintf('Training data: n1=%d, n2=%d.', x$n1, x$n2))
+    message(sprintf('Number of feature block(s): %d.', length(x$paras)))
 }
